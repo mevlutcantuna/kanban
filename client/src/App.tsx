643df919eac3b72baad91e6f1a770c1253287293
mevlutcantuna <mttuna90@gmail.com>
2022-06-21 11:30:16 +0300

    components created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Signup />} path='/signup' />
+          <Route element={<Login />} path='/login' />
+          <Route element={<Home />} path='/' />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
