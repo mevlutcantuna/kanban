3bf359c16639f9ec1b3df5dfb06202c9dffcb6a6
mevlutcantuna <mttuna90@gmail.com>
2022-08-02 16:41:38 +0300

    project is completed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,8 +6,11 @@ import 'antd/dist/antd.min.css';
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const uri = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/graphql' : 'https://kanban-graphql-server.herokuapp.com/graphql'
+
+console.log(process.env.NODE_ENV)
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/graphql',
+  uri: uri,
   cache: new InMemoryCache(),
   connectToDevTools: true
 });
