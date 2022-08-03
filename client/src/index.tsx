99fdfc8ac5350b8e17d55ac8d0fcc5227e406686
mevlutcantuna <mttuna90@gmail.com>
2022-08-03 09:00:25 +0300

    a bug solved in index.js

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,9 +6,10 @@ import 'antd/dist/antd.min.css';
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const uri = process.env.NODE_ENV !== 'development' ? 'http://localhost:8080/graphql' : 'https://kanban-graphql-server.herokuapp.com/graphql'
+const uri = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/graphql' : 'https://kanban-graphql-server.herokuapp.com/graphql'
 
 console.log(process.env.NODE_ENV)
+
 const client = new ApolloClient({
   uri: uri,
   cache: new InMemoryCache(),
